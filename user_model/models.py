887b4f96db0731e111683ec830da35d0b64de81b
OIITuMuCT <vataman@live.ru>
2025-01-13 14:45:20 +0300

    add admin site

--- user_model/models.py
+++ user_model/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    class Meta(AbstractUser.Meta):
+        db_table = 'custom_user'
+    mobile = models.CharField(max_length=15, blank=True)
